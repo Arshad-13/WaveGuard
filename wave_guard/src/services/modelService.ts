2e15f19285ac316548f02f42ea9c439715b92189
Aayush Prasad <aayushp336@gmail.com>
2025-08-31 09:02:40 +0530

    minor fix

--- wave_guard/src/services/modelService.ts
+++ wave_guard/src/services/modelService.ts
@@ -10,7 +10,7 @@ export interface TsunamiPredictionInput {
   latitude: number;
   longitude: number;
 }
-
+ 
 export interface TsunamiPredictionResponse {
   status: string;
   tsunami_prediction: {
